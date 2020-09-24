3aa707083e3628f0c3062738cfe788ab6d13842f
jcapellanvasquez <53913902+jcapellanvasquez@users.noreply.github.com>
2020-09-24 15:00:30 -0400

    add model book interface and AppSharedModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AppSharedModule} from './app-shared/app-shared.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AppSharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/book.ts
@@ -0,0 +1,8 @@
+export interface Book {
+  id: string;
+  title: string;
+  description: string;
+  likes: number;
+  createdDate: Date;
+  isNew: boolean;
+}
