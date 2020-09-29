384d3f8593958e6763b7344018a642cfbaea1a3d
jcapellanvasquez <53913902+jcapellanvasquez@users.noreply.github.com>
2020-09-28 22:22:28 -0400

    header and footer some style

--- src/app/app-shared/app-shared.module.ts
+++ src/app/app-shared/app-shared.module.ts
@@ -1,16 +1,24 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {InputTextModule} from 'primeng/inputtext';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import {RouterModule} from '@angular/router';
+import {ButtonModule} from 'primeng/button';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [NavbarComponent, FooterComponent],
   imports: [
     CommonModule,
     InputTextModule,
+    RouterModule,
+    ButtonModule,
   ],
   exports: [
-    InputTextModule
+    InputTextModule,
+    NavbarComponent,
+    FooterComponent
   ]
 })
 export class AppSharedModule {
